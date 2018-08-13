ec3b151e92d746cb5758a7f954064317acbf0c2d
lapinpt <pmmpmc@gmail.com>
2018-08-13 02:38:49 +0100

    magisk init script
    Modded for Xiaomi Redmi Note 5 PRO MIUI 10

--- unlock.sh
+++ unlock.sh
@@ -1,54 +1,45 @@
-#!/bin/sh
+#!/system/bin/sh
 #
 # Android pattern unlock
 # Author: Matt Wilson
+# Modified : Lapin
+# Last Modified : 13 Ago 2018
+# Note : Modded for Xiaomi Redmi Note 5 PRO MIUI 10
 # Licence: Free to use and share. If this helps you please buy me a beer :)
 #
-# This script sends simulated touch input over ADB for remotely swiping on Android's pattern lockscreen.
-# This allows you to unlock the device even if the touch screen is broken.
-#
-# Note: You must have USB debugging enabled on your device for this script to work (in the developer options).
-# This script will not work unless this option is enabled. I recommend turning it on to make life easier if you drop your phone.
-# Note: The device does not need to be rooted for this method to work.
-#
-# You need to have adb on your PATH to run this script.
-# E.g. on Mac add to ~/.bash_profile: export PATH="/Users/username/Library/Android/sdk/platform-tools:$PATH"
-#
-# Customise the variables in the top section of the script with your phone's unlock code and coordinates
-#
-# Usage:
-# chmod +x unlock.sh
-# ./unlock.sh
+# MAGISK init.d
+# nano '/sbin/.core/img/.core/service.d/initprops' 
+# sleep 20
+# /data/local/unlock.sh > /data/local/init.log
 
 # =======================================================================================================================
 
 # Variables
 #
-# Coordinates will vary depending on the screen resolution of your device (defaults are for Nexus 4 at 768x1280)
 # The pattern should be set based on the following layout:
 # 1 2 3
 # 4 5 6
 # 7 8 9
 
-PATTERN="1 2 3 6 5 4 7 8 9" # The unlock pattern to draw, space seperated
+PATTERN="3 1 7 9" # The unlock pattern to draw, space seperated
 
-COL_1=166                   # X coordinate of column 1 (in pixels)
-COL_2=384                   # X coordinate of column 2 (in pixels)
-COL_3=576                   # X coordinate of column 3 (in pixels)
+COL_1=245                   # X coordinate of column 1 (in pixels)
+COL_2=540                   # X coordinate of column 2 (in pixels)
+COL_3=830                   # X coordinate of column 3 (in pixels)
 
-ROW_1=553                   # Y coordinate of row 1 (in pixels)
-ROW_2=738                   # Y coordinate of row 2 (in pixels)
-ROW_3=984                   # Y coordinate of row 3 (in pixels)
+ROW_1=1250                   # Y coordinate of row 1 (in pixels)
+ROW_2=1550                   # Y coordinate of row 2 (in pixels)
+ROW_3=1830                   # Y coordinate of row 3 (in pixels)
 
-MULTIPLIER=2                # Multiplication factor for coordinates. For Nexus 4, set this to 2. For low res phones such as
+MULTIPLIER=1                # Multiplication factor for coordinates. For Nexus 4, set this to 2. For low res phones such as
                             # Samsung Galaxy S2, set this to 1. Experiment with this value if you can't see anything happening.
 
 WAKE_SCREEN_ENABLED=true    # If true, the script will start by sending the power button press event
 
 SWIPE_UP_ENABLED=true       # If true, the script will swipe upwards before drawing the pattern (e.g. for lollipop lockscreen)
-SWIPE_UP_X=450              # X coordinate for initial upward swipe. Only used if SWIPE_UP_ENABLED is true
-SWIPE_UP_Y_FROM=1000        # Start Y coordinate for initial upward swipe. Only used if SWIPE_UP_ENABLED is true
-SWIPE_UP_Y_TO=200           # End Y coordinate for initial upward swipe. Only used if SWIPE_UP_ENABLED is true
+SWIPE_UP_X=500              # X coordinate for initial upward swipe. Only used if SWIPE_UP_ENABLED is true
+SWIPE_UP_Y_FROM=1900        # Start Y coordinate for initial upward swipe. Only used if SWIPE_UP_ENABLED is true
+SWIPE_UP_Y_TO=500           # End Y coordinate for initial upward swipe. Only used if SWIPE_UP_ENABLED is true
 
 # =======================================================================================================================
 
@@ -76,32 +67,43 @@ Y[9]=$(( ${ROW_3} * ${MULTIPLIER} ))
 
 # Function definitions
 
+LockScreen() {
+  if [ "$(dumpsys power | grep 'Display Power' |  grep -oE '(ON|OFF)')" == "ON" ] ; then
+      echo "Screen is already on."
+      echo "Turning screen off."
+      input keyevent 26         # KEYCODE_POWER
+      sleep 1
+  fi
+}
+
 WakeScreen() {
 	if [ "$WAKE_SCREEN_ENABLED" = true ]; then
-		adb shell input keyevent 26
+		input keyevent 26           # KEYCODE_POWER
+		sleep 1
 	fi
 }
 
 SwipeUp() {
 	if [ "$SWIPE_UP_ENABLED" = true ]; then
-		adb shell input swipe ${SWIPE_UP_X} ${SWIPE_UP_Y_FROM} ${SWIPE_UP_X} ${SWIPE_UP_Y_TO}
+		input swipe ${SWIPE_UP_X} ${SWIPE_UP_Y_FROM} ${SWIPE_UP_X} ${SWIPE_UP_Y_TO} 400
+		sleep 1
 	fi
 }
 
 StartTouch() {
-	adb shell sendevent /dev/input/event2 3 57 14
+	sendevent /dev/input/event2 3 57 14
 }
 
 SendCoordinates () {
-	adb shell sendevent /dev/input/event2 3 53 $1
-	adb shell sendevent /dev/input/event2 3 54 $2
-	adb shell sendevent /dev/input/event2 3 58 57
-	adb shell sendevent /dev/input/event2 0 0 0
+	sendevent /dev/input/event2 3 53 $1
+	sendevent /dev/input/event2 3 54 $2
+	sendevent /dev/input/event2 1 330 1
+	sendevent /dev/input/event2 0 0 0
 }
 
 FinishTouch() {
-	adb shell sendevent /dev/input/event2 3 57 4294967295
-	adb shell sendevent /dev/input/event2 0 0 0
+	sendevent /dev/input/event2 3 57 4294967295
+	sendevent /dev/input/event2 0 0 0
 }
 
 SwipePattern() {
@@ -114,6 +116,7 @@ SwipePattern() {
 
 # Actions
 
+LockScreen
 WakeScreen
 SwipeUp
 StartTouch
